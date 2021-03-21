7d193d747a9ee63af5e91879b69008fa75d8e227
unknown <hconway@luc.edu>
2021-03-21 17:01:58 -0500

    follow along MANY TO ONE

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,16 +1,11 @@
 package org.launchcode.codingevents.models;
 
-import org.springframework.boot.convert.DataSizeUnit;
-
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.Objects;
 
 @Entity
 public class Event extends AbstractEntity {
@@ -39,25 +34,17 @@ public class Event extends AbstractEntity {
         this.eventCategory = eventCategory;
     }
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 
     public String getContactEmail() { return contactEmail; }
     public void setContactEmail(String contactEmail) { this.contactEmail = contactEmail; }
 
     public EventCategory getEventCategory() { return eventCategory; }
-    public void setType(EventCategory eventCategory) { this.eventCategory = eventCategory; }
+    public void setEventCategory(EventCategory eventCategory) { this.eventCategory = eventCategory; }
 
     @Override
     public String toString() {
